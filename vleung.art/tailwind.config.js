9ddf9e2de570c01b3e2e27f381345b6717db2783
vivian.leung <vivz753@gmail.com>
2021-12-04 15:34:11 -0800

    add pkg: tailwindcss and config files

---
+++ vleung.art/tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  purge: [
+    // Use *.tsx if using TypeScript
+    './pages/**/*.tsx',
+    './components/**/*.tsx'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
