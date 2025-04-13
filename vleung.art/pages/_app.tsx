976019a5fe375673f89bb834d16cf31bc5895295
vivian.leung <vivz753@gmail.com>
2025-04-12 20:16:56 -0700

    packages: remove emotion, add prettier, update deps

--- vleung.art/pages/_app.tsx
+++ vleung.art/pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import GlobalStyles from './../styles/GlobalStyles'
 import '../styles/globals.css'
 import Layout from '../src/components/layout'
 
@@ -8,7 +7,6 @@ const title="Vivian's Art"
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GlobalStyles />
       <Head>
         <title>{title}</title>
         <meta name="a site for the artsy fartsy collection" content="vivian's art" />
--- vleung.art/src/components/molecules/Rainbow.tsx
+++ vleung.art/src/components/molecules/Rainbow.tsx
@@ -1,83 +1,79 @@
-import '../atoms/Arc'
-import Arc from '../atoms/Arc'
-import { keyframes, css } from '@emotion/react'
+import "../atoms/Arc"
+import Arc from "../atoms/Arc"
 
 const Rainbow: React.FC = (props) => {
-
   // prefer if i can do this through ref on <path> in Arc
-    //  const pathLength = document.querySelector('path')?.getTotalLength(); // is there a way to do this without querying the document?
-  const pathLength = 314;
+  //  const pathLength = document.querySelector('path')?.getTotalLength(); // is there a way to do this without querying the document?
+  const pathLength = 314
 
-  const strokeDash = keyframes`
-    to {
-      stroke-dashoffset: ${pathLength};
-    }
-  `
-  // can be prop
-  const height = 50;
+  const height = 50
 
   // should be fixed
-  const viewBox = "25 40 250 100";
-  const center = 150;
-  const r = 100;
-  const lArcAngle = 270;
-  const rArcAngle = 90;
+  const viewBox = "25 40 250 100"
+  const center = 150
+  const r = 100
+  const lArcAngle = 270
+  const rArcAngle = 90
 
   return (
-    <div className="flex justify-center w-auto h-auto">
+    <div className="flex h-auto w-auto justify-center">
+      <style>
+        {`
+          .animate {
+            stroke-dasharray: ${pathLength};
+            animation: strokeDash 2s infinite alternate ease;
+          }
+          
+          @keyframes strokeDash {
+            to {
+                stroke-dashoffset: ${pathLength};
+            }
+          }
+        `}
+      </style>
       <div className="relative">
-      <div className="">
-        <Arc
-          viewBox={viewBox}
-          height={height}
-          x={center}
-          y={center}
-          radius={r}
-          startAngle={lArcAngle}
-          endAngle={rArcAngle}
-          cssStyle={[
-            css`stroke-dasharray: ${pathLength};`,
-            css`animation: ${strokeDash} 2s infinite alternate ease;`,
-            css`stroke: #FF1B8D`
-          ]}
-        />
-      </div>
-      <div className="absolute bottom-0">
-        <Arc
-          viewBox={viewBox}
-          height={height}
-          x={center}
-          y={center}
-          radius={r-20}
-          startAngle={lArcAngle}
-          endAngle={rArcAngle}
-          cssStyle={[
-            css`stroke-dasharray: ${pathLength};`,
-            css`animation: ${strokeDash} 2s infinite alternate ease;`,
-            css`stroke: #FFDA00`
-          ]}
-        />
-      </div>
-      <div className="absolute bottom-0">
-        <Arc
-          viewBox={viewBox}
-          height={height}
-          x={center}
-          y={center}
-          radius={r-40}
-          startAngle={lArcAngle}
-          endAngle={rArcAngle}
-          cssStyle={[
-            css`stroke-dasharray: ${pathLength};`,
-            css`animation: ${strokeDash} 2s infinite alternate ease;`,
-            css`stroke: #1BB3FF`
-          ]}
-        />
-      </div>
+        <div className="">
+          <Arc
+            viewBox={viewBox}
+            height={height}
+            x={center}
+            y={center}
+            radius={r}
+            startAngle={lArcAngle}
+            endAngle={rArcAngle}
+            css={{ stroke: `#FF1B8D` }}
+            className="animate"
+          />
+        </div>
+        <div className="absolute bottom-0">
+          <Arc
+            viewBox={viewBox}
+            height={height}
+            x={center}
+            y={center}
+            radius={r - 20}
+            startAngle={lArcAngle}
+            endAngle={rArcAngle}
+            css={{ stroke: `#FFDA00` }}
+            className="animate"
+          />
+        </div>
+        <div className="absolute bottom-0">
+          <Arc
+            viewBox={viewBox}
+            height={height}
+            x={center}
+            y={center}
+            radius={r - 40}
+            startAngle={lArcAngle}
+            endAngle={rArcAngle}
+            css={{ stroke: `#1BB3FF` }}
+            className="animate"
+          />
+        </div>
       </div>
     </div>
-     
   )
 }
 
-export default Rainbow;
+export default Rainbow
