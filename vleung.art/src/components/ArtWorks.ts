319d4cba2e76869fcb06bc9630bcdfd06caa05cd
vivian.leung <vivz753@gmail.com>
2025-04-13 23:08:54 -0700

    upload artwork images

---
+++ vleung.art/src/components/ArtWorks.ts
@@ -0,0 +1,183 @@
+export interface Art {
+  url: string
+  medium: "oil" | "gouache" | "charcoal" | "pencil" | "marker" | "acrylic" | "digital"
+  title: string
+}
+
+export const images: Art[] = [
+  // *** DIGITAL
+  {
+    url: "/images/art/plein-air-sunny-day_digital.png",
+    medium: "digital",
+    title: "Plein Air Sunny Day in digital",
+  },
+  {
+    url: "/images/art/plein-air-cloudy_digital.png",
+    medium: "digital",
+    title: "Plein Air Cloudy in digital",
+  },
+  {
+    url: "/images/art/plein-air-sunset_digital.png",
+    medium: "digital",
+    title: "Plein Air Sunset in digital",
+  },
+  {
+    url: "/images/art/eggy-pinky_digital.png",
+    medium: "digital",
+    title: "Eggy and Pinky in digital",
+  },
+  {
+    url: "/images/art/lighting-fundamentals-hw4_digital.png",
+    medium: "digital",
+    title: "Lighting 4 homework in digital",
+  },
+  {
+    url: "/images/art/lighting-fundamentals-hw3_digital.png",
+    medium: "digital",
+    title: "Lighting 3 homework in digital",
+  },
+  {
+    url: "/images/art/lighting-fundamentals-hw2_digital.png",
+    medium: "digital",
+    title: "Lighting 2 homework in digital",
+  },
+  {
+    url: "/images/art/lighting-fundamentals-hw1_digital.png",
+    medium: "digital",
+    title: "Lighting 1 homework in digital",
+  },
+  {
+    url: "/images/art/marianne_digital.png",
+    medium: "digital",
+    title: "Marianne in digital",
+  },
+  {
+    url: "/images/art/prafull-sawant_digital.png",
+    medium: "digital",
+    title: "Prafull Sawant study in digital",
+  },
+  {
+    url: "/images/art/baby-monkey_digital.png",
+    medium: "digital",
+    title: "baby monkey in digital",
+  },
+  {
+    url: "/images/art/queens-gambit_digital.png",
+    medium: "digital",
+    title: "queens gambit in digital",
+  },
+  // {
+  // 	url: "/images/art/utah_digital.png",
+  // 	medium: "digital",
+  // 	title: "friends in utah in digital",
+  // },
+  // {
+  // 	url: "/images/art/raven-starfire_digital.jpg",
+  // 	medium: "digital",
+  // 	title: "raven and star fire in digital",
+  // },
+  // {
+  // 	url: "/images/art/little-nightmarees_digital.png",
+  // 	medium: "digital",
+  // 	title: "little nightmares in digital",
+  // },
+
+  // *** TRADITIONAL
+  {
+    url: "/images/art/duck_oil.png",
+    medium: "oil",
+    title: "duck in oil",
+  },
+  {
+    url: "/images/art/jose-doge_oil.png",
+    medium: "oil",
+    title: "Jose as a doge in oil",
+  },
+  {
+    url: "/images/art/banana_oil.png",
+    medium: "oil",
+    title: "banana in oil",
+  },
+  {
+    url: "/images/art/lady-portrait_oil.jpg",
+    medium: "oil",
+    title: "lady portrait in oil",
+  },
+  {
+    url: "/images/art/mimi-portrait_oil.png",
+    medium: "oil",
+    title: "Mimi portrait in oil",
+  },
+  {
+    url: "/images/art/playful-cat_oil.png",
+    medium: "oil",
+    title: "playful cat in oil",
+  },
+  {
+    url: "/images/art/gary-gudetama_gouache.png",
+    medium: "gouache",
+    title: "Gary and Gudetama in gouache",
+  },
+  {
+    url: "/images/art/mimi-drop-paint_acrylic.png",
+    medium: "acrylic",
+    title: "mimi in acrylic",
+  },
+  {
+    url: "/images/art/ruth-bader-ginsberg_oil.jpg",
+    medium: "oil",
+    title: "Ruth Bader Ginsberg in oil",
+  },
+  {
+    url: "/images/art/anthony-bourdain_oil.jpg",
+    medium: "oil",
+    title: "Anthony Bourdain in oil",
+  },
+  {
+    url: "/images/art/froggy_gouache.jpg",
+    medium: "gouache",
+    title: "frog in gouache",
+  },
+  {
+    url: "/images/art/vintage-ray_gouache.jpg",
+    medium: "gouache",
+    title: "portrait in gouache",
+  },
+  // {
+  //   url: "/images/art/back-muscles_charcoal.jpg",
+  //   medium: "charcoal",
+  //   title: "back muscles in charcoal",
+  // },
+  {
+    url: "/images/art/child-portrait_charcoal.jpg",
+    medium: "charcoal",
+    title: "child portrait in charcoal",
+  },
+  {
+    url: "/images/art/man-portrait_charcoal.png",
+    medium: "charcoal",
+    title: "man portrait in charcoal",
+  },
+  {
+    url: "/images/art/nude-model_charcoal.jpg",
+    medium: "charcoal",
+    title: "nude model in charcoal",
+  },
+  // {
+  //   url: "/images/art/old-man_charcoal.jpg",
+  //   medium: "charcoal",
+  //   title: "old man in charcoal",
+  // },
+  {
+    url: "/images/art/teen-portrait_charcoal.jpg",
+    medium: "charcoal",
+    title: "teen portrait in charcoal",
+  },
+  // {
+  //   url: "/images/art/skull.jpg",
+  //   medium: "marker",
+  //   title: "skull in marker",
+  // },
+]
+
+export default images
